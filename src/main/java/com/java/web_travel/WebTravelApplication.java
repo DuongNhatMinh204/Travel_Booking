9498ac56e21e38038716c1780af94fb876041dd1
thangdv <duongthang.1903@gmail.com>
2025-02-12 01:04:04 +0700

    update

--- src/main/java/com/java/web_travel/WebTravelApplication.java
+++ src/main/java/com/java/web_travel/WebTravelApplication.java
@@ -2,11 +2,12 @@ package com.java.web_travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.awt.*;
 import java.net.URI;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class WebTravelApplication {
 
 	public static void main(String[] args) {
